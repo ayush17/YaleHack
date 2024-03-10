73b1f1b48c8daa266b20f9d213cb46018bd8bd81
ayush17 <ayushtiwari17011995@gmail.com>
2024-03-10 19:23:23 -0400

    railway configuration

--- src/index.js
+++ src/index.js
@@ -1,32 +1,32 @@
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const config = require("./config/config");
-const uri =${{shared.mongodb_uri}};
-// const uri=config.mongodb_uri
+const uri = config.mongodb_uri;
 const app = express();
 
-const roomRoute = require('./routes/roomsRoute.js')
+const roomRoute = require("./routes/roomsRoute.js");
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/rooms", roomRoute);
 
-
 app.listen(config.port, () => {
   console.log(`Server is running on port ${config.port}`);
 });
 
-
 async function run() {
   try {
-    mongoose.connect(uri).then(()=>{
-      console.log("Pinged your deployment. You successfully connected to MongoDB!")
-    })
+    const uri=`${{shared.mongodb_uri}}`
+    mongoose.connect(uri).then(() => {
+      console.log(
+        "Pinged your deployment. You successfully connected to MongoDB!"
+      );
+    });
   } catch {
-    await mongoose.connection.close()
+    await mongoose.connection.close();
   }
 }
 run().catch(console.dir);
